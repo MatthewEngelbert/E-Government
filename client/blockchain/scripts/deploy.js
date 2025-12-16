4f70bcfde531122d900607bac9a1936c0ed4fe4b
MatthewEngelbert <mattheweng2110@gmail.com>
2025-12-16 22:52:12 +0700

    Deployed the blockchain

--- client/blockchain/scripts/deploy.js
+++ client/blockchain/scripts/deploy.js
@@ -1,7 +1,10 @@
 async function main() {
   const Contract = await ethers.getContractFactory("DigitalDocumentRegistry");
-  const contract = await Contract.deploy();
 
+  const contract = await Contract.deploy({
+    gasLimit: 8_000_000
+  });
+  
   await contract.waitForDeployment();
 
   console.log("Contract deployed to:", await contract.getAddress());
